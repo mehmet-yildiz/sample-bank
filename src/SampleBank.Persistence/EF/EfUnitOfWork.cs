e63246d4e0744702c611a37459f26b0184de676e
Mehmet YILDIZ <mehmet-yildiz@msn.com>
2021-05-05 12:32:53 +0300

    ef unit of work implemented

---
+++ src/SampleBank.Persistence/EF/EfUnitOfWork.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using SampleBank.Core.Abstractions.Persistence;
+
+namespace SampleBank.Persistence.EF
+{
+    public class EfUnitOfWork : IUnitOfWork
+    {
+        //TODO: uncommented after database provider (commented for in-memory EF)
+        /*private readonly DbContext _dbContext;
+
+        public EfUnitOfWork(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _transaction = _dbContext.Database.BeginTransaction();
+        }
+        */
+
+        private IDbContextTransaction _transaction;
+
+        public void BeginTransaction()
+        {
+            //TODO: uncommented after database provider (commented for in-memory EF)
+            //_transaction = _dbContext.Database.BeginTransaction();
+        }
+
+        public void Commit()
+        {
+            if (_transaction == null)
+            {
+                return;
+            }
+
+            try
+            {
+                _transaction.Commit();
+                _transaction.Dispose();
+            }
+            finally
+            {
+                _transaction = null;
+            }
+        }
+
+        public void Rollback()
+        {
+            if (_transaction == null)
+            {
+                return;
+            }
+            _transaction.Rollback();
+        }
+
+        public void Dispose()
+        {
+            if (_transaction == null)
+            {
+                return;
+            }
+            _transaction.Dispose();
+        }
+    }
+}
