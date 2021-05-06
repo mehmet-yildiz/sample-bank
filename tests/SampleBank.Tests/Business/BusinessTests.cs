68c01e47c65fddd71dc9869370719edb9eb8afe6
Mehmet YILDIZ <mehmet-yildiz@msn.com>
2021-05-07 02:58:06 +0300

    added business tests

---
+++ tests/SampleBank.Tests/Business/BusinessTests.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq;
+using AutoFixture.Xunit2;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using SampleBank.Business;
+using SampleBank.Core.Abstractions.Business;
+using SampleBank.Core.Abstractions.Logging;
+using SampleBank.Core.Entity;
+using SampleBank.Persistence;
+using SampleBank.Persistence.EF;
+using Xunit;
+
+namespace SampleBank.Tests.Business
+{
+    public class BusinessTests
+    {
+        private readonly IBusinessBase<Customer> _service;
+
+        public BusinessTests()
+        {
+            var dbContext = new EfContext(new DbContextOptionsBuilder<EfContext>().Options);
+            var uow = new EfUnitOfWork();
+            var logger = new Logger();
+            var repo = new RepositoryBase<Customer>(new EfRepository(dbContext));
+            _service = new BusinessCustomer(repo, uow, logger);
+        }
+
+        [Theory, AutoData]
+        public void GetAllCustomers_should_return_expected_result(int expectedId)
+        {
+            _service.Insert(new Customer { Id = expectedId, Name = "mehmet", Surname = "yildiz" });
+            _service.Insert(new Customer { Id = 1, Name = "nurgul", Surname = "dereli" });
+
+            //when
+            var result = _service.GetAll();
+
+            //then
+            result.Data.First().Id.Should().Be(expectedId);
+            result.Data.Count().Should().Be(2);
+        }
+
+        [Theory, AutoData]
+        public void GetById_should_return_expected_result(int expectedId)
+        {
+            _service.Insert(new Customer { Id = expectedId, Name = "mehmet", Surname = "yildiz" });
+
+            //when
+            var result = _service.GetById(expectedId);
+
+            //then
+            result.Data.Id.Should().Be(expectedId);
+            result.Data.Id.Should().NotBe(expectedId + 1);
+        }
+
+    }
+}
