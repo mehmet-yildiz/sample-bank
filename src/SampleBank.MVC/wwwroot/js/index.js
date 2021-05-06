42db11dca4fc29504f8deec3ba2246a09382194b
Mehmet YILDIZ <mehmet-yildiz@msn.com>
2021-05-06 21:40:23 +0300

    completed->mvc

---
+++ src/SampleBank.MVC/wwwroot/js/index.js
@@ -0,0 +1,113 @@
+﻿(function () {
+    document.getElementById('login-button').addEventListener('click', function (e) {
+        this.innerText = "Please wait...";
+        this.setAttribute('disabled', 'disabled');
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+
+        if (username == "" || password == "") {
+            this.innerText = "Login";
+            this.removeAttribute('disabled');
+            alert("Fill username and password");
+            return;
+        }
+        const model = { username: username, password: password };
+
+        Common.Ajax("post", "auth/login", JSON.stringify(model), function (apiResponse) {
+            if (apiResponse.hasError === true) {
+                alert("An error occured. " + apiResponse.errorMessage);
+                return;
+            }
+
+            if (apiResponse.data?.token != null) {
+                localStorage.setItem("sampleBankToken", apiResponse.data.token);
+                closeModal("loginModal");
+            }
+        });
+    });
+
+    document.getElementById('btnOpenAccount').addEventListener('click', function (e) {
+        this.innerText = "Please wait...";
+        this.setAttribute('disabled', 'disabled');
+        const customerId = document.getElementById("customerId").value;
+        const initialCredit = document.getElementById("initialCredit").value;
+
+        if (customerId == "" || initialCredit == "") {
+            this.innerText = "Open Account";
+            this.removeAttribute('disabled');
+            alert("Fill Customer Id and Initial Credit");
+            return;
+        }
+        const model = { customerId: customerId, initialCredit: initialCredit };
+
+        Common.Ajax("post", "account/open", JSON.stringify(model), function (apiResponse) {
+            if (apiResponse.hasError === true) {
+                alert("An error occured. " + apiResponse.errorMessage);
+                return;
+            }
+
+            if (apiResponse.data === true) {
+                document.getElementById("result-section").innerText = "";
+                alert("Account opened. Check Account & Transaction List");
+                closeModal("openAccountModal");
+            }
+        });
+    });
+
+
+    document.getElementById('btnShowCustomers').addEventListener('click', function (e) {
+        this.innerText = "Please wait...";
+        this.setAttribute('disabled', 'disabled');
+        const thisBtn = this;
+
+        Common.Ajax("get", "customer/list", null, function (apiResponse) {
+            thisBtn.innerText = "Show Customers";
+            thisBtn.removeAttribute('disabled');
+            if (apiResponse.hasError === true) {
+                alert("An error occured. " + apiResponse.errorMessage);
+                return;
+            }
+
+            document.getElementById("result-section").innerText = JSON.stringify(apiResponse.data, null, "\t");
+        });
+    });
+
+
+    document.getElementById('btnShowAccounts').addEventListener('click', function (e) {
+        this.innerText = "Please wait...";
+        this.setAttribute('disabled', 'disabled');
+        const thisBtn = this;
+
+        Common.Ajax("get", "account/list", null, function (apiResponse) {
+            thisBtn.innerText = "Show Accounts";
+            thisBtn.removeAttribute('disabled');
+            if (apiResponse.hasError === true) {
+                alert("An error occured. " + apiResponse.errorMessage);
+                return;
+            }
+
+            document.getElementById("result-section").innerText = JSON.stringify(apiResponse.data, null, "\t");
+        });
+    });
+
+
+    document.getElementById('btnShowTransactions').addEventListener('click', function (e) {
+        this.innerText = "Please wait...";
+        this.setAttribute('disabled', 'disabled');
+        const thisBtn = this;
+
+        Common.Ajax("get", "transaction/list", null, function (apiResponse) {
+            thisBtn.innerText = "Show Transactions";
+            thisBtn.removeAttribute('disabled');
+            if (apiResponse.hasError === true) {
+                alert("An error occured. " + apiResponse.errorMessage);
+                return;
+            }
+
+            document.getElementById("result-section").innerText = JSON.stringify(apiResponse.data, null, "\t");
+        });
+    });
+})();
+
+
+
---
+++ src/SampleBank.MVC/wwwroot/js/site.js
@@ -0,0 +1,60 @@
+﻿function showLoader() {
+
+}
+
+function hideLoader() {
+
+}
+
+
+function closeModal(modalId) {
+    var loginModalEl = document.getElementById(modalId);
+    var modal = bootstrap.Modal.getInstance(loginModalEl);
+    modal.hide();
+}
+
+
+var Common = {
+    BaseApiUrl: "https://localhost:44361/",
+    Token: function () {
+        return localStorage.getItem("sampleBankToken");
+    },
+    Ajax: function (httpMethod, url, data, successCallback, canShowLoader = true) {
+        if (canShowLoader) {
+            showLoader();
+        }
+
+        var request = new XMLHttpRequest();
+        request.open(httpMethod.toUpperCase(), Common.BaseApiUrl + url, true);
+
+        request.setRequestHeader("Accept", "application/json");
+        request.setRequestHeader("Content-type", "application/json");
+        if (Common.Token()) {
+            request.setRequestHeader("Authorization", "Bearer " + Common.Token());
+        }
+
+        request.onload = function () {
+            hideLoader();
+            if (this.status >= 200 && this.status < 400) {
+                // Success!
+                var data = JSON.parse(this.response);
+                if (successCallback) {
+                    successCallback(data);
+                }
+            } else {
+               
+            }
+        };
+
+        request.onerror = function (err) {
+            // There was a connection error of some sort
+            debugger;
+        };
+
+        request.send(data);
+
+    },
+    AjaxErrorCallback: function (error, type, httpStatus) {
+
+    },
+};
